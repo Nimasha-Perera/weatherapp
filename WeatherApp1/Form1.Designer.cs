caade58c9e070f07f6b0f31ffa3cc98ed160dbb3
Nimasha-Perera <nethmiperera686@gmail.com>
2021-03-25 16:45:05 +0530

    my project

---
+++ WeatherApp1/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace WeatherApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtcity = new System.Windows.Forms.TextBox();
+            this.txtmaxtemp = new System.Windows.Forms.TextBox();
+            this.txtmintemp = new System.Windows.Forms.TextBox();
+            this.txtwindm = new System.Windows.Forms.TextBox();
+            this.txtwindk = new System.Windows.Forms.TextBox();
+            this.txthumidity = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtcountry = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "City";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Max temp";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Min temp";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(57, 290);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Wind Speed Kph";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(57, 345);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Humidity";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(57, 235);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(114, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Wind speed mph";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // txtcity
+            // 
+            this.txtcity.Location = new System.Drawing.Point(251, 64);
+            this.txtcity.Name = "txtcity";
+            this.txtcity.Size = new System.Drawing.Size(264, 22);
+            this.txtcity.TabIndex = 7;
+            this.txtcity.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtmaxtemp
+            // 
+            this.txtmaxtemp.Location = new System.Drawing.Point(251, 122);
+            this.txtmaxtemp.Name = "txtmaxtemp";
+            this.txtmaxtemp.Size = new System.Drawing.Size(264, 22);
+            this.txtmaxtemp.TabIndex = 8;
+            // 
+            // txtmintemp
+            // 
+            this.txtmintemp.Location = new System.Drawing.Point(251, 177);
+            this.txtmintemp.Name = "txtmintemp";
+            this.txtmintemp.Size = new System.Drawing.Size(264, 22);
+            this.txtmintemp.TabIndex = 9;
+            // 
+            // txtwindm
+            // 
+            this.txtwindm.Location = new System.Drawing.Point(251, 230);
+            this.txtwindm.Name = "txtwindm";
+            this.txtwindm.Size = new System.Drawing.Size(264, 22);
+            this.txtwindm.TabIndex = 10;
+            // 
+            // txtwindk
+            // 
+            this.txtwindk.Location = new System.Drawing.Point(251, 285);
+            this.txtwindk.Name = "txtwindk";
+            this.txtwindk.Size = new System.Drawing.Size(264, 22);
+            this.txtwindk.TabIndex = 11;
+            // 
+            // txthumidity
+            // 
+            this.txthumidity.Location = new System.Drawing.Point(251, 345);
+            this.txthumidity.Name = "txthumidity";
+            this.txthumidity.Size = new System.Drawing.Size(264, 22);
+            this.txthumidity.TabIndex = 12;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(856, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(288, 260);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(975, 44);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 17);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Cloud";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(978, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 41);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "show";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(60, 422);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1098, 238);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(571, 678);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 40);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "SHOW";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtcountry
+            // 
+            this.txtcountry.Location = new System.Drawing.Point(856, 19);
+            this.txtcountry.Name = "txtcountry";
+            this.txtcountry.Size = new System.Drawing.Size(288, 22);
+            this.txtcountry.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1196, 730);
+            this.Controls.Add(this.txtcountry);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txthumidity);
+            this.Controls.Add(this.txtwindk);
+            this.Controls.Add(this.txtwindm);
+            this.Controls.Add(this.txtmintemp);
+            this.Controls.Add(this.txtmaxtemp);
+            this.Controls.Add(this.txtcity);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Weather API";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtcity;
+        private System.Windows.Forms.TextBox txtmaxtemp;
+        private System.Windows.Forms.TextBox txtmintemp;
+        private System.Windows.Forms.TextBox txtwindm;
+        private System.Windows.Forms.TextBox txtwindk;
+        private System.Windows.Forms.TextBox txthumidity;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtcountry;
+    }
+}
+
